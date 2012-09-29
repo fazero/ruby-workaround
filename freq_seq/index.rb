92e76cd7b7145f8737dbdcb75e12ae00f5366aa1
Ramil Ziganshin <fazero@gmail.com>
2012-09-29 21:07:05 +0400

    minor changes

--- freq_seq/index.rb
+++ freq_seq/index.rb
@@ -1,11 +1,11 @@
 def freq_seq(n)
   if n > 0
     seq = "1"
-    (1...n).each{|i| seq = seq.scan(/(1{1,3}|2{1,3}|3{1,3})/).flatten.map{|element|element.size.to_s + element[0]}.join}
+    (1...n).each{seq = seq.scan(/(1{1,3}|2{1,3}|3{1,3})/).flatten.map{|element|element.size.to_s + element[0]}.join}
   else
     seq = nil
   end
   seq
 end
 
-p freq_seq(10) #=>"13211311123113112211"
+#p freq_seq(10) #=>"13211311123113112211"
